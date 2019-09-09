f16368d072687e66d8a7743ae5772b8f2eebf4a5
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2019-09-09 12:38:49 -0400

    5th

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.post("/",function(req,res){
   res.redirect("/");
 });
 
-app.listen(procee.env.PORT||3000,function(){
+app.listen(process.env.PORT||3000,function(){
   console.log("server started!");
 });
