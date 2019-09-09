f18c7bd2390187ab8e8aa397f5d97cf8bd1f377e
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2019-09-09 17:16:16 -0400

    a commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 let items=[];
-
+let workitems=[];
 let today=new Date();
 let options={
   weekday:"long",
@@ -20,13 +20,23 @@ let options={
 let day=today.toLocaleDateString("en-US",options);
 
 app.get("/",function(req,res){
-  res.render("todolist",{day:day,items:items});
+  res.render("todolist",{listTitle:day,items:items});
 });
 
 app.post("/",function(req,res){
   let item=req.body.next_item;
   items.push(item);
   res.redirect("/");
+});
+
+app.get("/work",function(req,res){
+  res.render("todolist",{listTitle:"work list",items:workitems});
+});
+
+app.post("/work",function(req,res){
+  let item=req.body.next_item;
+  workitems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(process.env.PORT||3000,function(){
