bc666bf814df4ee6def5065b1beb40dbe3029a67
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2019-09-09 12:23:02 -0400

    second commit

--- app.js
+++ app.js
@@ -7,23 +7,24 @@ const request=require("request");
 const app=express();
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
-var items=[];
+let items=[];
 
-var today=new Date();
-var options={
+let today=new Date();
+let options={
   weekday:"long",
   day:"numeric",
   month:"long"
 };
-var day=today.toLocaleDateString("en-US",options);
+let day=today.toLocaleDateString("en-US",options);
 
 app.get("/",function(req,res){
   res.render("todolist",{day:day,items:items});
 });
 
 app.post("/",function(req,res){
-  var item=req.body.next_item;
+  let item=req.body.next_item;
   items.push(item);
   res.redirect("/");
 });
